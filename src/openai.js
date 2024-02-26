6e51cb6f3097d5e4a042cd18fb1c031c880016a4
Diego Mirhan <mirhan.diego@gmail.com>
2024-02-26 16:57:53 -0300

    changed gpt api call

--- src/openai.js
+++ src/openai.js
@@ -19,13 +19,13 @@ const setupChatGptHandler = () => {
                 }],
                 temperature: 0.7,
             })
+
+
             return response.choices[0].message.content
         } catch (error) {
             return error
         }
     })
 }
-
-console.log(setupChatGptHandler())
 
 module.exports = { setupChatGptHandler }
