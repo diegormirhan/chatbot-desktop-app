48430b69f8d5b8248d9f68e9d4d97dc427809817
Diego Mirhan <mirhan.diego@gmail.com>
2024-03-08 14:35:07 -0300

    created and implemented logic fro apikey updates

---
+++ src/updateApiKey.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv")
+
+function updateApiKey() {
+  const app = express();
+  app.use(express.json());
+
+  app.listen(3000, () => {
+    console.log("--> API KEY UPDATE SERVER RUNNING ON PORT 3000");
+  });
+
+  app.post("/update-api", (req, res) => {
+    const apiKey = req.body.apiKey;
+    const envPath = path.join(__dirname, '..', '.env');
+    let envContent = fs.readFileSync(envPath, "utf-8");
+    envContent = envContent.replace(
+      /OPENAI_API_KEY=.*/,
+      `OPENAI_API_KEY=${apiKey}`
+    );
+    fs.writeFileSync(envPath, envContent, "utf-8");
+    dotenv.config({ path: envPath });
+    res.send("API KEY UPDATED");
+    console.log("--> API KEY UPDATED");
+  });
+}
+
+module.exports = { updateApiKey };
